3d8e4eb462e3ccb4444c07603ac0b94af4bccf9e
williamqsntdev <quartz_09kudzus@icloud.com>
2024-02-05 00:03:02 +0100

    remove pro

--- app/(platform)/(dashboard)/(routes)/organization/[organizationId]/billing/page.tsx
+++ app/(platform)/(dashboard)/(routes)/organization/[organizationId]/billing/page.tsx
@@ -1,7 +1,6 @@
 import { checkSubscription } from "@/lib/subscription"
 import { Separator } from "@/components/ui/separator";
 
-import { SubscriptionButton } from "./_components/subscription-button";
 
 import { Info } from "../_components/info";
 
@@ -12,9 +11,6 @@ const BillingPage = async () => {
     <div className="w-full">
       <Info isPro={isPro} />
       <Separator className="my-2" />
-      <SubscriptionButton
-        isPro={isPro}
-      />
     </div>
   );
 };
